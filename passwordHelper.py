1520fd8c176729ab339a3f38763ca93bc5507232
Alec <teranalec@gmail.com>
2026-03-15 00:27:31 -0700

    Arguments for function execution

--- passwordHelper.py
+++ passwordHelper.py
@@ -2,60 +2,68 @@
 import hashlib 
 import re 
 import sys 
+import secrets
 
 """
-A Python script to help verify the strength of passwords 
-I tried this in C but then I realized Python is probably
-the better language for this 
-
-Not doing anything low-level (yet)
+Putting something else here later 
 
 """
 
 
 def introMessage():
-    print("Welcome to astolica's password helper (^__^)")
-    print("Give me a password for me to check!")\
+    print("Welcome to Schrift")
+    print("Press 1 for password verification\n"
+    "or 2 for password salting and storage via SQL")
     
 
-def passwordAnalyzer(password):
+def passwordAnalyzer():
+    password = input("Enter your password: ")
     results = [] # analyze password and return results based on its nature
     if re.search(r'[\\",<>&:]', password):
-        results.append()
+        results.append(f"'{password} contains an invalid character (ex. '\, &, <>, etc.) Please remove it.")
 
     if len(password) < 16: # check if password is less than 16 (reccomended length)
-        results.append(f"'{password}' is too short, make it at least 16 characters. (-_-)\n")
+        results.append(f"'{password}' is too short, make it at least 16 characters.\n")
     else: 
-        results.append(f"'{password}' is lengthy! Good! (¬‿¬ )")
+        results.append(f"'{password}' is of adequate length.")
 
     if not re.search(r'[A-Z]', password): # look for capital letters in password 
         results.append("Missing uppercase letter(s), add one to make your password more secure.\n") 
     else:
-        results.append("Uppercase letter(s) detected, good! (¬‿¬ )")
+        results.append("Goos use of uppercase letter(s) detected.")
 
     if not re.search(r'[0-9]', password): # look for numbers 0-9 in password string 
         results.append("You don't have a number in this password, be sure to add one.\n")
     else:
-        results.append("Number(s) detected, good. Add multiple to increase entropy.(¯▿¯)")
+        results.append("Number(s) detected. Be sure to add multiple to increase entropy.")
     
     if not re.search(r'[\D\W]', password):
-        results.append(f"There's no special character in this password, add one now! (#`Д´)")
+        results.append(f"There's no special character in this password, add one.")
     else: 
-        results.append(f"Good use of special characters")
+        results.append(f"Good use of special characters.")
 
-
-        
     header = f"\n--- Password Analysis: '{password}' ---\n" # Setup for a pretty return function
     footer = "-" * len(header)
     return f"{header}\n" + "\n".join(results) + f"\n{footer}\n"
 
+def hashAndSalt(password: str):
+    salt = secrets.token_hex(16)
+
+
 def main(): # main function 
     introMessage()
-    givenPassword = input("> ")
-    print(f"Your password was '{givenPassword}', let's check it shall we? (o_O)\n")
-    print(passwordAnalyzer(givenPassword))
+    choice = input("> ")
+    if choice == "1":
+        passwordAnalyzer()
+    elif choice == "2":
+        hashAndSalt()
+    else: 
+        print("No other valid parameter was given. Aborting now.")
+        sys.exit()
+
     
     
+
 
 
 if __name__ == "__main__":
